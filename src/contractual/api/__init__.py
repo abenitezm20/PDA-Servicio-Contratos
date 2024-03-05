e9fc2782cc0403117a1af77e390d88b9ccf71e81
DanielOchoaSuarez <d.ochoas@uniandes.edu.co>
2024-03-04 22:21:35 -0500

    API y configuración proyecto

---
+++ src/contractual/api/__init__.py
@@ -0,0 +1,35 @@
+from flask import Flask, jsonify
+from contractual.conf.errors import ApiError
+from contractual.conf.db import init_db
+from .contratos import ab
+
+app = Flask(__name__)
+app.secret_key = '1D7FC7F9-3B7E-4C40-AF4D-141ED3F6013A'
+init_db()
+app.register_blueprint(ab)
+
+
+@app.errorhandler(ApiError)
+def handle_exception(err):
+    response = {
+        "msg": err.description
+    }
+    return jsonify(response), err.code
+
+
+def comenzar_consumidor():
+    import threading
+    import contractual.modulos.contratos.infraestructura.consumidores as contratos
+
+    # Suscripción a eventos
+    threading.Thread(target=contratos.suscribirse_a_eventos).start()
+    threading.Thread(target=contratos.suscribirse_a_eventos,
+                     args=[app]).start()
+
+    # Suscripción a comandos
+    # threading.Thread(target=contratos.suscribirse_a_comandos).start()
+    threading.Thread(target=contratos.suscribirse_a_comandos,
+                     args=[app]).start()
+
+
+comenzar_consumidor()
---
+++ src/contractual/api/contratos.py
@@ -0,0 +1,62 @@
+from flask import Blueprint, Response, request, session
+from contractual.modulos.contratos.aplicacion.queries.obtener_contratos import ObtenerPropiedadContratos
+from contractual.modulos.contratos.aplicacion.comandos.crear_propiedad_contratos import RegistrarPropiedadContratos
+from contractual.modulos.contratos.aplicacion.mapeadores import MapeadorPropiedadContratosDTOJson
+from contractual.seedwork.aplicacion.queries import ejecutar_query
+from contractual.seedwork.aplicacion.comandos import ejecutar_comando
+
+from contractual.modulos.contratos.infraestructura.despachadores import Despachador
+from contractual.modulos.contratos.infraestructura.schema.v1.comandos import ComandoRegistrarArrendamiento
+
+
+ab = Blueprint('contratos', __name__)
+
+
+@ab.route('/contratos/health', methods=['GET'])
+def health():
+    return Response({'result': 'OK'}, status=200, mimetype='application/json')
+
+
+@ab.route('/contratos/propiedad/<id>', methods=['GET'])
+def obtener_propiedad_contratro(id=None):
+    if id:
+        query_resultado = ejecutar_query(ObtenerPropiedadContratos(id))
+        map_propiedad = MapeadorPropiedadContratosDTOJson()
+        return map_propiedad.dto_a_externo(query_resultado.resultado)
+    else:
+        return Response({'message': 'GET'})
+
+
+@ab.route('/contrato', methods=['POST'])
+def registrar_propiedad_contratro():
+    session['uow_metodo'] = 'db'
+    propiedad_dict = request.json
+    map_propiedad = MapeadorPropiedadContratosDTOJson()
+    propiedad_dto = map_propiedad.externo_a_dto(propiedad_dict)
+    comando = RegistrarPropiedadContratos(
+        propiedad_dto.numero_contrato,
+        propiedad_dto.fecha_actualizacion,
+        propiedad_dto.fecha_creacion,
+        propiedad_dto.propiedad_id,
+    )
+
+    print('Comando crear contrato inicio')
+    ejecutar_comando(comando)
+    print('Comando crear contrato fin')
+
+    return Response('{}', status=202, mimetype='application/json')
+
+
+@ab.route('/async-contrato', methods=['POST'])
+def registrar_propiedad_contratro_async():
+    map_propiedad = MapeadorPropiedadContratosDTOJson()
+    arriendo_dto = map_propiedad.externo_a_dto(request.json)
+
+    # comando = ComandoRegistrarArrendamiento(
+    #     propiedad_id=arriendo_dto.propiedad_id,
+    #     numero_contrato=arriendo_dto.numero_contrato,
+    #     fecha_creacion=arriendo_dto.fecha_creacion,
+    #     fecha_actualizacion=arriendo_dto.fecha_actualizacion,)   
+
+    Despachador().publicar_comando(arriendo_dto, 'comandos-contrato')
+    return Response('{}', status=202, mimetype='application/json')
